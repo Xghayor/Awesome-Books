70ba7de98f521155669268ba8083d0062b69e5ed
Binyam Y. Olango <125895809+binyamolango@users.noreply.github.com>
2023-07-11 16:59:19 +0300

    Add style to the project

--- index.js
+++ index.js
@@ -5,26 +5,12 @@ const awesomeBooks = document.getElementById('book');
 
 // Add books function
 
-function updateBooks() {
-  awesomeBooks.innerHTML = '';
-
-  books.forEach((book) => {
-    awesomeBooks.innerHTML += `
-      <div>
-        <h2>${book.title}</h2>
-        <p>${book.author}</p>
-        <button type="button" class="remove-button" value="${book.title}">Remove</button>
-      </div>
-      <hr>
-    `;
-  });
-}
-
 function add() {
   const title = document.getElementById('fTitle').value;
   const author = document.getElementById('fAuthor').value;
 
   if (title.trim() === '' || author.trim() === '') {
+    alert('Please enter title and author');
     return;
   }
 
@@ -36,17 +22,30 @@ function add() {
 
 addBtn.addEventListener('click', add);
 
+function updateBooks() {
+  awesomeBooks.innerHTML = '';
+
+  books.forEach(book => {
+    awesomeBooks.innerHTML += `
+      <div class="books">
+        <p class="bookDetail">"${book.title}" by ${book.author}</p>
+        <button type="button" class="remove-button" value="${book.title}">Remove</button>
+      </div>
+    `;
+  });
+}
+
 // Remove books function
 function removeBook(bookTitle) {
-  books = books.filter((book) => book.title !== bookTitle);
+books = books.filter(book => book.title !== bookTitle);
   localStorage.setItem('books', JSON.stringify(books));
   updateBooks();
 }
 
-awesomeBooks.addEventListener('click', (event) => {
-  const bookTitle = event.target.value;
-  removeBook(bookTitle);
+awesomeBooks.addEventListener('click', function(event) {
+    const bookTitle = event.target.value;
+    removeBook(bookTitle);
 });
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', function() {
   updateBooks();
 });
