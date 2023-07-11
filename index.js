6a866abe6cf7bc4e41fde577e37c5297eb1ea3f1
Xghayor <cc.baqir@gmail.com>
2023-07-11 10:48:43 +0100

    Add a Add books function

---
+++ index.js
@@ -0,0 +1,39 @@
+let books = JSON.parse(localStorage.getItem('books')) || [];
+
+const addBtn = document.getElementById('formBtn');
+const awesomeBooks = document.getElementById('book');
+
+// Add books function
+
+function add() {
+  const title = document.getElementById('fTitle').value;
+  const author = document.getElementById('fAuthor').value;
+
+  if (title.trim() === '' || author.trim() === '') {
+    alert('Please enter title and author');
+    return;
+  }
+
+  books.push({ title, author });
+  localStorage.setItem('books', JSON.stringify(books));
+
+  updateBooks();
+}
+
+addBtn.addEventListener('click', add);
+
+function updateBooks() {
+  awesomeBooks.innerHTML = '';
+
+  books.forEach(book => {
+    awesomeBooks.innerHTML += `
+      <div>
+        <h2>${book.title}</h2>
+        <p>${book.author}</p>
+        <button type="button" class="remove-button" value="${book.title}">Remove</button>
+      </div>
+      <hr>
+    `;
+  });
+}
+
