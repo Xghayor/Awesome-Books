af855e8ae07c9a8a4dcebfd690269d824fa217bf
Xghayor <cc.baqir@gmail.com>
2023-07-12 19:06:38 +0100

    Fix linter errors

--- display.js
+++ display.js
@@ -6,43 +6,48 @@ const formLink = document.getElementById('form-link');
 const contactLink = document.getElementById('contact-link');
 
 listLink.addEventListener('click', () => {
-    if (!addNew.classList.contains('hidden')) {
-        addNew.classList.add('hidden');
-    }
+  if (!addNew.classList.contains('hidden')) {
+    addNew.classList.add('hidden');
+  }
 
-    if (!contact.classList.contains('hidden')) {
-        contact.classList.add('hidden');
-    }
+  if (!contact.classList.contains('hidden')) {
+    contact.classList.add('hidden');
+  }
 
-    if (list.classList.contains('hidden')) {
-        list.classList.remove('hidden');
-    }
+  if (list.classList.contains('hidden')) {
+    list.classList.remove('hidden');
+  }
 });
 
 formLink.addEventListener('click', () => {
-    if (!list.classList.contains('hidden')) {
-        list.classList.add('hidden');
-    }
+  if (!list.classList.contains('hidden')) {
+    list.classList.add('hidden');
+  }
 
-    if (!contact.classList.contains('hidden')) {
-        contact.classList.add('hidden');
-    }
+  if (!contact.classList.contains('hidden')) {
+    contact.classList.add('hidden');
+  }
 
-    if (addNew.classList.contains('hidden')) {
-        addNew.classList.remove('hidden');
-    }
+  if (addNew.classList.contains('hidden')) {
+    addNew.classList.remove('hidden');
+  }
 });
 
 contactLink.addEventListener('click', () => {
-    if (!list.classList.contains('hidden')) {
-        list.classList.add('hidden');
-    }
+  if (!list.classList.contains('hidden')) {
+    list.classList.add('hidden');
+  }
 
-    if (!addNew.classList.contains('hidden')) {
-        addNew.classList.add('hidden');
-    }
+  if (!addNew.classList.contains('hidden')) {
+    addNew.classList.add('hidden');
+  }
 
-    if (contact.classList.contains('hidden')) {
-        contact.classList.remove('hidden');
-    }
+  if (contact.classList.contains('hidden')) {
+    contact.classList.remove('hidden');
+  }
 });
+
+const date = new Date();
+const currentTime = document.getElementById('get-time');
+
+currentTime.innerHTML = date;
